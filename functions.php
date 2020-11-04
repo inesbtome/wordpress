c1c87263386a0c2b9f531ad4256c93e855a10338
inesbtome <66378038+inesbtome@users.noreply.github.com>
2020-11-04 17:11:02 +0000

    add redirect logo

--- functions.php
+++ functions.php
@@ -6,3 +6,16 @@ function my_login_stylesheet() {
     wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
