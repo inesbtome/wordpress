d9390983a512b4d9693df8fabceea2487d5d140b
inesbtome <66378038+inesbtome@users.noreply.github.com>
2020-11-04 16:53:01 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,1 @@
 
-//Custom Login Page
-
-function my_login_stylesheet() {
-    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
-    wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
-}
-add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
