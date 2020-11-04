ac8f690585f3b9ca92b7226342030e6b2cb9ede3
inesbtome <66378038+inesbtome@users.noreply.github.com>
2020-11-04 17:12:48 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,3 +19,20 @@ function my_login_logo_url_title() {
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
+//Remove Dashboard Widgtes for NON Admin Users
+
+function remove_dashboard_meta() {
+      if ( ! current_user_can( 'manage_options' ) ) {
+            remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+            remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+            remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+      }
+}
+add_action( 'admin_init', 'remove_dashboard_meta' );
+
